0c754be453fedb780c2182f9d395ce00556d20b3
keshew <artyom.korotckov2017@yandex.ru>
2025-11-15 12:49:45 +0500

    Initial Commit

---
+++ CasinoSlotsSat/CasinoSlotsSatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoSlotsSatApp.swift
+//  CasinoSlotsSat
+//
+//  Created by Артём Коротков on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasinoSlotsSatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
